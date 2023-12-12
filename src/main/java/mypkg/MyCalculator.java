286880c35ec728523f87dfc626bd84a2cd225605
Hrithik53 <kumarhrithik53@gmail.com>
2023-12-12 12:11:12 +0530

    Commit

---
+++ src/main/java/mypkg/MyCalculator.java
@@ -0,0 +1,41 @@
+package mypkg;
+
+public class MyCalculator {
+	
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
+	public int div(int a, int b)
+	{
+		return a/b;
+	}
+	public void display()
+	{
+		System.out.println("Welcome GFG");
+	}
+	public void display2()
+	{
+		System.out.println("Welcome GFG");
+	}
+	public void display3()
+	{
+		System.out.println("Welcome GFG3");
+	}
+
+	public static void main(String[] args) {
+		
+		MyCalculator ob = new MyCalculator();
+		System.out.println("Sum is: "+ob.sum(20, 10));
+		System.out.println("Sum is: "+ob.sub(20, 10));
+	}
+}
